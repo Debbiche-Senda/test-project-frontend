1faf25041f62ddd3bb4e79680934ec52312f298c
Debbiche-Senda <debbiche.senda.douda@gmail.com>
2022-03-10 13:18:06 +0100

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import Chat from './components/Chat';
-import Clients from './components/Clients';
-import Draw from './components/Draw';
-import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
